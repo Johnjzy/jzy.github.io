867996a73334f4e31dd5b761a93227aca2d9f29f
jiangzy26 <jiangzhiyong12@outlook.com>
2021-07-23 20:10:50 +0800

    Create First_c.c

---
+++ First_c.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int dogs;
+    printf("How many dosg do you have ?\n");
+    scanf("%d", &dogs);
+    printf("So you have %d dog(s)~\n", dogs);
+    return 0;
+}
+
+
